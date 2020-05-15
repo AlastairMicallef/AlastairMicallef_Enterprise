9f0ca46c4dcfc37c05462ece6c46606f704b9af3
Chaos1994 <mezigan@gmail.com>
2020-05-15 08:13:34 +0200

    Created the Qaulity Model

---
+++ AlastairMicallef_Enterprise/Models/QualityModels.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AlastairMicallef_Enterprise.Models
+{
+    public class QualityModels
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        public string Qualityame { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+    }
+}
